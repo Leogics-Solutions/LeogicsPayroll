0d9c7776134fbe1f171ed0e894125a52c29d0f87
whuteann <annmak.shiroann@gmail.com>
2025-12-25 16:53:54 +0800

    Use environment variable for Firebase credentials

--- payroll_mvp/firebase.py
+++ payroll_mvp/firebase.py
@@ -1,7 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate('firebase-credentials.json')
+# Check if credentials are in environment variable (production)
+firebase_creds = os.environ.get('FIREBASE_CREDENTIALS')
+
+if firebase_creds:
+    # Production: load from environment variable
+    cred_dict = json.loads(firebase_creds)
+    cred = credentials.Certificate(cred_dict)
+else:
+    # Local development: load from file
+    cred = credentials.Certificate('firebase-credentials.json')
+
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
